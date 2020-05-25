aa4bd1e8ef437c78760cb00a9f562e387b79e18b
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-25 19:44:21 +0530

    Stored the Daily Wage along with the Total Wage

---
+++ uc8.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+echo Welcome to Employee Wage Computation Program
+wageperHour=20
+dailyHours=8
+fullTime=1
+partTime=2
+workDays=20
+maxHours=100
+totalEmpHours=0
+days=1
+
+function workHours()
+{
+        case $empCheck in
+                $fullTime)
+                        dailyHours=8
+                        ;;
+                $partTime)
+                        dailyHours=4
+                        ;;
+                *)
+                        dailyHours=0
+                        ;;
+        esac
+}
+
+while [ $totalEmpHours -le $maxHours -a $days -le $workDays ]
+do
+	empCheck=$((RANDOM%3))
+	((days++))
+	workHours $empCheck
+	totalEmpHours=$(($totalEmpHours+$dailyHours))
+	dailyWage[$days]=$(($dailyHours*$wageperHour))
+done
+empWageperMonth=$(($totalEmpHours*$wageperHour))
+echo "Employee Wage for month : Rs. $empWageperMonth "
+echo ${dailyWage[@]}
