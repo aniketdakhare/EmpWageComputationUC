726467d8981a17e92369d1b12ae1f6779c123668
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-25 17:04:22 +0530

    Calculated Wages for 20 days month or for 100 hour

---
+++ uc6.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo Welcome to Employee Wage Computation Program
+wageperHour=20
+dailyHours=8
+fullTime=1
+partTime=2
+workDays=20
+maxHours=100
+days=1
+for (( i=0 ; $days<=$workDays ; i++ ))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$fullTime)
+			dailyHours=8
+			((days++))
+			;;
+		$partTime)
+			dailyHours=4
+			((days++))
+			;;
+		*)
+			dailyHours=0
+			;;
+	esac
+	completeHours=$(($completeHours+$dailyHours))
+	empWage=$(($dailyHours*$wageperHour))
+	empWageperMonth=$(($empWageperMonth+$empWage))
+	if (( $completeHours >= $maxHours ))
+	    then
+                echo "Total working Hours = 100"
+                break
+        fi
+
+done
+echo "Employee Wage for month : Rs. $empWageperMonth "
