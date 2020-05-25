32ddbfa276ad5b3c30b2d4c4f29e806a4b1a4e4c
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-25 18:51:21 +0530

    Monthly Wages Calculated

--- uc5.sh
+++ uc5.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash-x
 
 echo Welcome to Employee Wage Computation Program
 wageperHour=20
@@ -6,22 +6,22 @@ dailyHours=8
 fullTime=1
 partTime=2
 workDays=20
-empCheck=$((RANDOM%3))
-case $empCheck in
-	$fullTime)
-		echo "Fulltime Employee"
-		dailyHours=8
-		;;
-	$partTime)
-		echo "Part Time Employee"
-		dailyHours=4
-		;;
-	*)
-		echo "Employee is Absent"
-		dailyHours=0
-		;;
-esac
-empWage=$(($dailyHours*$wageperHour))
-empWageperMonth=$(($empWage*$workDays))
+for (( day=1; day<=workDays; day++))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$fullTime)
+			dailyHours=8
+			;;
+		$partTime)
+			dailyHours=4
+			;;
+		*)
+			dailyHours=0
+			;;
+	esac
+	empWage=$(($dailyHours*$wageperHour))
+	empWageperMonth=$(($empWageperMonth+$empWage))
+done
 echo "Employee Wage for month : Rs. $empWageperMonth "
 
